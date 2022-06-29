10d4dd33f0f170fc0f3f5b5948ca5d1967a457d6
deleteapplephones <jeb.jugalt@gmail.com>
2022-06-29 09:35:00 +0100

    Update

---
+++ src/Verbatim.lua
@@ -0,0 +1,11 @@
+local Verbatim = {};
+
+local function GetModules(_parent: Instance)
+    local _root = {};
+    for _, _child in pairs(_parent) do
+        
+    end;
+    return _root;
+end
+
+return Verbatim;
